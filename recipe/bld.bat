6ebdf33c8c314656b0ec7f5b10c1d1a5a2eee737
memento <claude.rc@gmail.com>
2024-01-19 16:10:20 -0600

    DBG: fullpath in SOURCES.txt?

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,8 @@
 call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
 if errorlevel 1 exit 1
 
+rm -r coincurve.egg-info/SOURCES.txt
+rm -r libsecp256k1
+
 %PYTHON% -m pip install . --no-deps --ignore-installed -vv
 exit 0
