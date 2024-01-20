73224b8ea685209b1487d1d09128d4e98f1dce84
memento <claude.rc@gmail.com>
2024-01-20 09:13:47 -0600

    DBG: MANIFEST.in to remove abspath?

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,7 @@
 
 set -eox pipefail
 
-rm -rf ${SRC_DIR}/{coincurve.egg-info, libsecp256k1}
+rm -rf ${SRC_DIR}/libsecp256k1
+rm -rf ${SRC_DIR}/coincurve.egg-info
 
 ${PYTHON} -m pip install . --no-deps --ignore-installed -vv
